8fee499346ad4cebfbb7ecdb291881e2cd55452e
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-22 23:10:38 +0600

    Started with addEntry().

---
+++ script.js
@@ -0,0 +1,10 @@
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value}.input-container`);
+}
