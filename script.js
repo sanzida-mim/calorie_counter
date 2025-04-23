b6b740523e0e3f5b7b81e95e8961039b905beb3f
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-23 23:50:45 +0600

    created html block to be updated dynamically

--- script.js
+++ script.js
@@ -19,4 +19,13 @@ function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value}.input-container`);
 
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+
+    const HTMLString = `
+    <label for="${entryDropdown}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    
+    <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>`
 }
