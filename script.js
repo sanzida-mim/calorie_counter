a06038d599f5daaf9c01598fbc6b4a3ff43743a3
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-29 23:38:58 +0600

    Displaying HTML Output for Surplus or Deficit Results.

--- script.js
+++ script.js
@@ -57,4 +57,12 @@ function calculateCalories(e) {
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
 
     const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+
+    output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted.</p>
+    <p>${budgetCalories} Calories Consumed.</p>
+    <p>${budgetCalories} Calories Burned.</p>`;
+
+    output.classList.remove("hide");
 }
