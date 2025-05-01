1ca70beb5b6b75c6cf850de5dfb70c2c96badc0c
sanzida-mim <mim.3ingenious@gmail.com>
2025-05-01 23:57:48 +0600

    Updated clearForm().

--- script.js
+++ script.js
@@ -86,10 +86,9 @@ function getCaloriesFromInputs(list) {
 
 function clearForm() {
     const inputContainers = Array.from(document.querySelectorAll('.input-container'));
-   
-   
+
     for (const container of inputContainers) {
-      container.innerHTML = '';
+        container.innerHTML = '';
     }
 
     budgetNumberInput.value = '';
